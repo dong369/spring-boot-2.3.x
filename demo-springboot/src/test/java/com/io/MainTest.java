d8910acb0906c23cbde214e7edc01ba23db8a778
guodd <18838177689@163.com>
2020-08-31 09:16:55 +0800

    spring-boot-2.3.x源码

---
+++ demo-springboot/src/test/java/com/io/MainTest.java
@@ -0,0 +1,16 @@
+package com.io;
+
+import org.junit.Test;
+
+/**
+ * The class/interface
+ *
+ * @author guodd
+ * @version 1.0 use jdk 1.8
+ */
+public class MainTest {
+	@Test
+	public void test(){
+		System.out.println("jav");
+	}
+}
